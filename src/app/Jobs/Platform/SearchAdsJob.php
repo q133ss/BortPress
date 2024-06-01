a63ad51e9d01de6dee71e274e1002dbc262c7540
Alexey <test@git.ru>
2024-06-02 02:26:02 +0300

    notification service

--- src/app/Jobs/Platform/SearchAdsJob.php
+++ src/app/Jobs/Platform/SearchAdsJob.php
@@ -3,6 +3,7 @@
 namespace App\Jobs\Platform;
 
 use App\Models\Ad;
+use App\Services\NotificationService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -29,5 +30,26 @@ class SearchAdsJob implements ShouldQueue
     public function handle(): void
     {
         // Ищем похожее и создаем уведомление
+        $results = Ad::where('type_id', $this->ad->type_id)
+        ->where('inventory', $this->ad->inventory)
+        ->where('pay_format', $this->ad->pay_format)
+        ->where('start_date', '>=' , $this->ad->start_date)
+        ->where('end_date', '<=', $this->ad->end_date)
+        ->where('region_id', $this->ad->region_id)
+        ->count();
+
+        if($results != 0){
+            // Создаем уведомление
+            $title = 'Новое совпадение!';
+            $text = "Найдено $results совпадений по вашему запросу";
+            $link = env('APP_URL')."/offers?type_id=".$this->ad->type_id.
+            "&inventory=".$this->ad->inventory.
+            "&pay_format=".$this->ad->pay_format.
+            "&start_date=".$this->ad->start_date.
+            "&end_date=".$this->ad->end_date.
+            "&region_id=".$this->ad->region_id;
+
+            (new NotificationService())->create($title, $text, [$this->ad->user_id], $link);
+        }
     }
 }
---
+++ src/app/Services/NotificationService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Notification;
+
+class NotificationService
+{
+    public function create(string $title, string $text, array $user_id, $link = null): true
+    {
+        foreach ($user_id as $id) {
+            Notification::create([
+                'title'   => $title,
+                'text'    => $text,
+                'link'    => $link,
+                'user_id' => $id
+            ]);
+        }
+
+        return true;
+    }
+}
